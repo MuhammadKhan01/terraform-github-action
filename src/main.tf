b1e6634518ec63875212b90de39647aab3ba334a
Muhammad Khan <Muhammad.Khan@illion.com.au>
2022-03-30 08:06:26 +1100

    Added new Terraform Block

---
+++ src/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+ backend "azurerm" {
+    resource_group_name  = "demo-rg-mak"
+    storage_account_name = "sttfstatemgt01"
+    container_name       = "tfstatemak"
+    key                  = "demo.terraform.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+# Create a resource group
+
+resource "azurerm_resource_group" "rgmak" {
+    name     = "demo-rgmak"
+    location = "aus-east"
+
+    tags = {
+        environment = "TF_Test_Sub_Mak"
+    }
+}
